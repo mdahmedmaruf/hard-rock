4d8eb27d9f8d1632eb6f48df17511877083b1a03
Maruf Ahmed <mdahmedmaruf@gmail.com>
2020-08-19 11:47:50 +0600

    update app js file

--- app.js
+++ app.js
@@ -1,100 +1,62 @@
-const searchSong = document.getElementById('searchSong');
-const searchSongBtn = document.getElementById('searchSongBtn');
-const songSuggestionList = document.getElementById("songSuggestionList");
-const lyricsSongSuggestionList = document.getElementById("lyricsSongSuggestionList");
+const searchButton = document.getElementById("search-button");
+const searchResult = document.getElementById("lyrics-search-results");
+const fullLyrics = document.getElementById("full-lyrics");
 
-// lyrics suggestion
-searchSong.addEventListener("keypress", event => {
-    if (searchSong.value.length > 0) {
-        searchSong.style.color = "black";
-    }
+searchButton.addEventListener("click", function () {
+	const searchInput = document.getElementById("search-input").value;
 
-    fetch(`https://api.lyrics.ovh/suggest/${event.target.value+event.key}`)
-        .then(res => res.json())
-        .then(data => {
-            for (let i = 1; i <= 5; i++) {
-                document.getElementById("title" + i).innerText = data.data[i].title;
-                document.getElementById("artist" + i).innerText = data.data[i].artist.name;
-
-                let title = data.data[i].title;
-                let artist = data.data[i].artist.name;
-                document.getElementById("lyricsBtn" + i).addEventListener("click", (event) => {
-                    let api2 = `https://api.lyrics.ovh/v1/${artist}/${title}`;
-                    fetch(api2)
-                        .then(res => res.json())
-                        .then(data => {
-                            let str = data.lyrics.split(" ");
-                            let [a, b, c] = str;
-                            if (str.length > 10) {
-                                document.getElementById("lyricsContentTitle").innerText = `${a} ${b} ${c}`;
-                            } else {
-                                document.getElementById("lyricsContentTitle").innerText = `${a}`;
-                            }
-                            document.getElementById("textContent").innerText = data.lyrics;
-                        });
-                    document.getElementById("lyricsContent").style.display = "block";
-                });
-            }
-            songSuggestionList.style.display = "block";
-        });
+	if (searchInput) {
+		fetch(`https://api.lyrics.ovh/suggest/${searchInput}/`)
+			.then((response) => response.json())
+			.then((data) => getSearchResult(data));
+	} else {
+		alert("Please add song title");
+	}
+	document.getElementById("search-input").value = "";
 });
 
-// all matched lyrics
+function getSearchResult(search) {
+	searchResult.innerHTML = "";
+	for (let i = 0; i < 10; i++) {
+		let songTitle = search.data[i].title;
+		let singerName = search.data[i].artist.name;
+		let albumImage = search.data[i].artist.picture_medium;
 
-searchSongBtn.addEventListener('click', event => {
-    if (searchSong.value.length < 1) {
-        let searchSong = document.getElementById("searchSong");
-        searchSong.value = "type a lyrics name";
-        searchSong.style.color = "red";
-        document.getElementById("lyricsSongSuggestionList").style.display = "none";
-    } else {
-        searchSong.style.color = "black";
-        let api = `https://api.lyrics.ovh/suggest/${searchSong.value}`;
-        document.getElementById("lyricsContent").style.display = "none";
-        for (let i = 1; i <= 10; i++) {
-            document.getElementById("textContent" + i).style.display = "none";
-        }
-        fetch(api)
-            .then(res => res.json())
-            .then(data => {
-                if (data.data.length == 0) {
-                    document.getElementById("alertUnavailable").innerHTML = `<h4 style="color: #c0392b; text-align:center">Lyrics not available.</h4>`;
-                } else {
-                    for (let i = 1; i < data.data.length; i++) {
+		let result = `<div class="single-result row align-items-center my-3 p-3">
+                <div class="col-md-2 album-image">
+                    <img src="${albumImage}" alt="${singerName}">
+                </div>
+                <div class="col-md-7">
+                    <h3 class="lyrics-name">${songTitle}</h3>
+                    <p class="author lead">Album by <span>${singerName}</span></p>
+                </div>
+                <div class="col-md-3 text-md-right text-center">
+                    <button class="btn btn-success" onclick="getLyrics('${singerName}', '${songTitle}')">Get Lyrics</button>
+                </div>
+            </div>`;
+		searchResult.innerHTML += result;
+	}
+}
 
-                        document.getElementById("lyricsTitle" + i).innerHTML = data.data[i].title;
-                        document.getElementById("lyricsArtist" + i).innerHTML = data.data[i].artist.name;
+/* Getting lyrics from API */
 
-                        lyricsSongSuggestionList.style.display = "block";
-                        let count = 1;
-                        document.getElementById("getLyricsBtn" + i).addEventListener("click", event => {
+function getLyrics(singerName, songTitle) {
+	fetch(`https://api.lyrics.ovh/v1/${singerName}/${songTitle}`)
+		.then((response) => response.json())
+		.then((data) => {
+			fullLyrics.innerHTML = `
+                        <button id="back-button" class="btn go-back text-white" onClick="goBack()">&lsaquo; go back</button>
+                        <h2 class="text-success mb-4">${singerName} - ${songTitle}</h2>
+                        <pre class="lyric text-white">${
+													!data.lyrics ? data.error : data.lyrics
+												}</pre>
+                    `;
+			searchResult.style.display = "none";
+		});
+}
 
-                            let textContent = document.getElementById("textContent" + i);
-
-                            let title = data.data[i].title;
-                            let artist = data.data[i].artist.name;
-                            let api2 = `https://api.lyrics.ovh/v1/${artist}/${title}`;
-                            fetch(api2)
-                                .then(res => res.json())
-                                .then(data => {
-                                    if (data.lyrics == undefined) {
-                                        textContent.innerHTML = `<p style="color: #c0392b; text-align:center;">The lyrics of this song is not available currently</p>`;
-                                        textContent.style.display = "block";
-                                    } else {
-                                        if (count % 2 !== 0) {
-                                            textContent.innerHTML = data.lyrics;
-                                            textContent.style.display = "block";
-                                            count++;
-                                        } else {
-                                            textContent.style.display = "none";
-                                            count++;
-                                        }
-                                    }
-                                });
-                        });
-                    }
-                }
-            });
-        songSuggestionList.style.display = "none";
-    }
-});
+/* Back to search result */
+function goBack() {
+	searchResult.style.display = "block";
+	fullLyrics.innerHTML = "";
+}
